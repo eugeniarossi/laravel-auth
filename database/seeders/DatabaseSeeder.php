b13aaaadfb8c60d5e84b39167b456d7a23ed896f
eugeniarossi <eugenia.rossi@outlook.com>
2023-06-04 17:08:12 +0200

    feat: Project model & seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // per eseguire tutti i seeder insieme li specifico: 
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            ProjectSeeder::class
+        ]);
     }
 }
