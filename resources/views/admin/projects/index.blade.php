04cddb7d1ebd817e511ebddb51de13268bfa8202
eugeniarossi <eugenia.rossi@outlook.com>
2023-06-05 12:15:22 +0200

    fix: index & show views

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2>Lista dei progetti</h2>
+<div class="container my-3">
+    <h2 class="my-4">Lista dei progetti</h2>
     <table class="table">
         <thead>
             <tr>
@@ -20,15 +20,15 @@
             <td>{{ $project->content }}</td>
             <td>{{ $project->slug }}</td>
             <td>
-                <ul>
+                <ul class="list-unstyled">
                     <li>
-                        <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-sm btn-primary">Show</a>
+                        <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-sm btn-primary my-1">Show</a>
                     </li>
                     <li>
-                        <a href="" class="btn btn-sm btn-warning">Edit</a>
+                        <a href="" class="btn btn-sm btn-warning my-1">Edit</a>
                     </li>
                     <li>
-                        <a href="" class="btn btn-sm btn-primary">Delete</a>
+                        <a href="" class="btn btn-sm btn-danger my-1">Delete</a>
                     </li>
                 </ul>
             </td>
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2>{{ $project->title }}</h2>
+<div class="container my-3">
+    <h3 class="my-3">{{ $project->title }}</h3>
     <p>{{ $project->content }}</p>
 </div>
 @endsection
